b7bf8f0a14e52e663e1737131ef64677cb1a73d0
Yin Yin Kyaw <yinyinkyaw.zotefamily@gmail.com>
2024-01-03 11:42:50 +0630

    create order form

---
+++ ProcessOrder.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bob's auto parts - order results</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>bob's auto part</h1>
+        <h2>order result</h2>
+        <div class="result">
+            <?php
+            echo "<p>order process at: </p>";
+            echo "<span class='date'>";
+            echo date('H:i, j F Y') . "</span><br /><br />"
+            ?>
+            <?php
+
+            $tires = 0;
+            $oil = 0;
+            $spark = 0;
+            $totalQty = 0;
+
+            if (isset($_POST["txtTires"]) && !empty($_POST['txtTires'])) {
+                $tires = $_POST["txtTires"];
+            }
+
+            if (isset($_POST["txtOil"]) && !empty($_POST["txtOil"])) {
+                $oil = $_POST["txtOil"];
+            }
+
+            if (isset($_POST["txtSpark"]) && !empty($_POST["txtSpark"])) {
+                $spark = $_POST["txtSpark"];
+            }
+
+            $totalQty = $tires + $oil + $spark;
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
